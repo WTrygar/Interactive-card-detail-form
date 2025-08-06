cbb3c85c5565f5e964655d2c4bd02ceeb4b39e8a
WTrygar <chandelier.wt@gmail.com>
2025-08-06 12:20:13 +0200

    feat: CardBack basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Component } from 'vue'
+import CardBack from './components/CardBack.vue'
 import CardFront from './components/CardFront.vue'
 
 interface Props {
@@ -15,6 +16,7 @@ const props = defineProps<Props>()
   <div class="min-h-lvh max-auto relative flex flex-col content-center">
     <div class="bg-[url('/src/images/bg-main-mobile.png')] bg-no-repeat bg-cover h-180">
       <CardFront />
+      <CardBack />
     </div>
   </div>
 </template>
--- src/components/CardBack.vue
+++ src/components/CardBack.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-
+  <div
+    class="flex flex-col bg-[url('/src/images/bg-card-back.png')] bg-contain bg-no-repeat h-70 w-120 p-8 font-grotesk text-white tracking-[.25em]"
+  >
+    <div class="w-10/10 text-sm text-right pt-22 pr-6">123</div>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
